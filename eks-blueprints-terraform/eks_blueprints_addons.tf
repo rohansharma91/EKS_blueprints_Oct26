9939253f91f6c7582009f7efe788153d047d82ee
rohansharma91 <115484779+rohansharma91@users.noreply.github.com>
2024-10-26 11:48:24 +0530

    Update eks_blueprints_addons.tf

--- eks-blueprints-terraform/eks_blueprints_addons.tf
+++ eks-blueprints-terraform/eks_blueprints_addons.tf
@@ -22,7 +22,7 @@ module "eks_blueprints_addons" {
   cluster_version   = module.eks.cluster_version
   oidc_provider_arn = module.eks.oidc_provider_arn
 
-  enable_aws_load_balancer_controller = true
+  enable_aws_load_balancer_controller = false
   enable_metrics_server               = true
   enable_karpenter = true
   karpenter = {
